5817a91914b7400c5ea347f91499926a22d5084c
Donghe-Zhu <donzhu@student.ethz.ch>
2020-11-16 22:40:37 +0100

    Observation and Model Comparison

--- code/plotting_functions.R
+++ code/plotting_functions.R
@@ -1,6 +1,5 @@
 # define global map function (annual)
 map_global <- function(df, var) {
-
   name_var <- var
   var <- sym(var)
 
@@ -12,7 +11,12 @@ map_global <- function(df, var) {
     geom_raster(aes(lon, lat, fill = !!var)) +
     scale_fill_viridis_c(name = name_var) +
     coord_quickmap(expand = 0) +
-    labs(title = paste(name_var, "Global Surface Annual Average Distribution"), subtitle = "Depth: 5m", x = "Longitude", y = "Latitude") +
+    labs(
+      title = paste(name_var, "Global Surface Annual Average Distribution"),
+      subtitle = paste("Depth: 5m | Year:", i_year),
+      x = "Longitude",
+      y = "Latitude"
+    ) +
     geom_raster(data = landmask,
                 aes(lon, lat), fill = "grey80")
 
@@ -21,7 +25,6 @@ map_global <- function(df, var) {
 
 # define global section function (annual)
 section_global <- function(df, var) {
-
   name_var <- var
   var <- sym(var)
 
@@ -31,24 +34,28 @@ section_global <- function(df, var) {
     ggplot(aes(dist, depth, z = !!var)) +
     geom_contour_filled() +
     geom_vline(data = section_global_coordinates %>% filter(lat == 0.5),
-               aes(xintercept = dist), col = "white") +
+               aes(xintercept = dist),
+               col = "white") +
     scale_fill_viridis_d(name = name_var) +
     coord_cartesian(expand = 0,
                     ylim = c(500, 0)) +
     scale_y_reverse() +
-    theme(axis.title.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          legend.position = "top") +
+    theme(
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      legend.position = "top"
+    ) +
     labs(y = "Depth (m)",
          title = "Global U-shape Section",
-         subtitle = "Annual average")
+         subtitle = paste("Annual average", i_year))
 
   deep <- df_sub %>%
     ggplot(aes(dist, depth, z = !!var)) +
     geom_contour_filled() +
     geom_vline(data = section_global_coordinates %>% filter(lat == 0.5),
-               aes(xintercept = dist), col = "white") +
+               aes(xintercept = dist),
+               col = "white") +
     scale_fill_viridis_d(name = name_var) +
     scale_y_reverse() +
     coord_cartesian(expand = 0, ylim = c(5400, 500)) +
@@ -62,7 +69,6 @@ section_global <- function(df, var) {
 
 # define zonal mean section function (annual)
 section_zonal <- function(df, i_basin_AIP, var) {
-
   name_var <- var
   var <- sym(var)
 
@@ -78,26 +84,33 @@ section_zonal <- function(df, i_basin_AIP, var) {
     ggplot(aes(lat, depth, z = zonal_mean)) +
     geom_contour_filled(breaks = breaks) +
     scale_fill_viridis_d(name = name_var) +
-    coord_cartesian(expand = 0,
-                    ylim = c(500, 0),
-                    xlim = c(lat_min, lat_max)) +
-    scale_y_reverse() +
-    theme(axis.title.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
+    coord_cartesian(
+      expand = 0,
+      ylim = c(500, 0),
+      xlim = c(lat_min, lat_max)
     ) +
-    labs(y = "Depth (m)",
-         title = "Zonal Annual Average Section",
-         subtitle = paste("Basin:", i_basin_AIP))
+    scale_y_reverse() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+    ) +
+    labs(
+      y = "Depth (m)",
+      title = "Zonal Annual Average Section",
+      subtitle = paste("Basin:", i_basin_AIP, "| Year:", i_year)
+    )
 
   deep <- df_sub %>%
     ggplot(aes(lat, depth, z = zonal_mean)) +
     geom_contour_filled(breaks = breaks) +
     scale_fill_viridis_d(name = name_var) +
     scale_y_reverse() +
-    coord_cartesian(expand = 0,
-                    ylim = c(5400, 500),
-                    xlim = c(lat_min, lat_max)) +
+    coord_cartesian(
+      expand = 0,
+      ylim = c(5400, 500),
+      xlim = c(lat_min, lat_max)
+    ) +
     labs(x = "latitude (°N)", y = "Depth (m)")
 
   surface / deep +
